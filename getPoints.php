2319db2a1ac5202993381b9520e74b7ff13b0e58
Christian Lucht <christian@christian-lucht.de>
2021-11-10 01:26:41 +0100

    php for getting json with sum of points of all teams

--- getPoints.php
+++ getPoints.php
@@ -1,1 +1,32 @@
 <?php
+$ini_array = parse_ini_file("config.ini");
+$dbhost = $ini_array['dbhost'];
+$dbuser = $ini_array['dbuser'];
+$dbpass = $ini_array['dbpass'];
+$db     = $ini_array['db'];
+
+$dbconnect=mysqli_connect($dbhost,$dbuser,$dbpass,$db);
+
+if ($dbconnect->connect_error) {
+    die("Database connection failed: " . $dbconnect->connect_error);
+}
+
+$participants_query = mysqli_query($dbconnect, "SELECT * FROM participants") or die (mysqli_error($dbconnect));
+
+$all = array();
+
+while ($row = mysqli_fetch_array($participants_query)) {
+
+    ${'team'.$row['participant_id'].'_query'} = mysqli_query($dbconnect, "SELECT SUM(points) AS sum FROM games WHERE winner_id LIKE ".$row['participant_id']) or die (mysqli_error($dbconnect));
+    ${'team'.$row['participant_id'].'_points'} = mysqli_fetch_array(${'team'.$row['participant_id'].'_query'});
+
+    ${$row['name']} = ${'team'.$row['participant_id'].'_points'}['sum'];
+    $all[$row['name']] = ${'team'.$row['participant_id'].'_points'}['sum'];
+
+}
+
+echo json_encode($all);
+
+
+mysqli_close($dbconnect);
+?>
